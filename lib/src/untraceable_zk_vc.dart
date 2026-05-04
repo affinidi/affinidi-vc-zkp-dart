8be8344c3c28fd0fe2ac530a0a8e769714570c62
Marco Attanasio <marco.a@affinidi.com>
2026-05-04 11:33:27 +0200

    chore: export ffi classes

--- lib/src/untraceable_zk_vc.dart
+++ lib/src/untraceable_zk_vc.dart
@@ -5,4 +5,5 @@ export 'issuer.dart' show VcIssuer;
 export 'issuer_public_key_parse.dart'
     show IssuerBabyJubCoords, tryParseIssuerBabyJubCommaSeparated;
 export 'models.dart' show Disclosure, SignedVcDocument, VcSignature;
+export 'rust_eddsa_helper_ffi.dart' show EddsaSignatureResult, RustEddsaHelperFfi;
 export 'verifier.dart' show VcVerifier, VerificationResult;
